d850820152c23ce6728dc2be0b0dc56be6332b88
Ab <mickyamb@gmail.com>
2022-09-14 15:20:44 -0400

    Agregado clase insumos y propiedades

---
+++ InsumosInformatica/Insumos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsumosInformatica
+{
+    internal class Insumos
+    {
+        public  int idInsumo { get; set; }
+        public string nombreInsumo { get; set; }
+        public int cantidadInsumo { get; set; }
+        public int costo { get; set; }
+    }
+}
--- InsumosInformatica/Program.cs
+++ InsumosInformatica/Program.cs
@@ -10,6 +10,7 @@ namespace InsumosInformatica
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
