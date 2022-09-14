9ec24e1c356b14f2bc3cfce4475dbd2c71168454
micaias.dev@outlook.com <micaias.dev@outlook.com>
2022-09-14 17:59:45 -0400

    Instancie la clase Insumo, en una lista de tipo insumo y logre hacer que ID sea incremental confome se agrega nuevos objetos. El problema es que la variable asignada era local no static

--- InsumosInformatica/Insumos.cs
+++ InsumosInformatica/Insumos.cs
@@ -8,9 +8,37 @@ namespace InsumosInformatica
 {
     internal class Insumos
     {
-        public  int idInsumo { get; set; }
-        public string nombreInsumo { get; set; }
-        public int cantidadInsumo { get; set; }
-        public int costo { get; set; }
+        //variable
+        private static int idProducto=0;
+        //Propiedades
+        public  int IdInsumo { get; set; }
+        public string NombreInsumo { get; set; }
+        public int CantidadInsumo { get; set; }
+        public int CostoInsumo { get; set; }
+
+   
+        //Constructor por defecto
+        public Insumos()
+        {
+            this.IdInsumo = CrearID();
+        }
+        //Consctructor parametrizado
+        public Insumos (string nombreInsumo, int cantidadInsumo, int costoInsumo)
+        {
+            this.IdInsumo = CrearID();
+            this.NombreInsumo = nombreInsumo;
+            this.CantidadInsumo = cantidadInsumo;
+            this.CostoInsumo = costoInsumo;
+        }
+        //Metodos
+        protected int CrearID()
+        {
+            return ++idProducto;
+        }
+        public override string ToString()
+        {
+            return String.Format("ID {0}\t-Descripcion {1}\t-TotalExistente: {2}\t-Costo Unitario: {3}\t-Costo Total {4}",
+                this.IdInsumo, this.NombreInsumo, this.CantidadInsumo, this.CostoInsumo, (this.CostoInsumo * this.CantidadInsumo));
+        }
     }
 }
--- InsumosInformatica/Program.cs
+++ InsumosInformatica/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,80 @@ namespace InsumosInformatica
 {
     internal class Program
     {
+        static List<Insumos> InsumosINformaticos = new List<Insumos> { new Insumos("tn285",10,180000), new Insumos("tn230", 8, 200000), new Insumos("tn1060", 14, 380000) };
         static void Main(string[] args)
         {
+            ConsoleKeyInfo teclaElegida;
+            do
+            {
+                MenuNav();
+                teclaElegida =Console.ReadKey();
+                if (teclaElegida.Key == ConsoleKey.D1)
+                {
+                    ListarInsumos();
+                }
+                else if(teclaElegida.Key == ConsoleKey.D2)
+                {
+                    AgregarInsumos();
+                }
+                else if (teclaElegida.Key == ConsoleKey.D3)
+                {
+                    EntregarInsumos();
+                }
+                else if (teclaElegida.Key == ConsoleKey.D4)
+                {
+                    EliminarInsumos();
+                }else
+                {
+                    Console.WriteLine("Opcion incorrecta:");
+                }
 
+            } while ( teclaElegida.Key != ConsoleKey.Escape);
+            
+            
+        }
+
+        static void MenuNav()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t###Sistema de Insumos Informatico###\n");
+            Console.WriteLine("Seleccione la opcion que desea realizar");
+            Console.WriteLine("1 - Listar Insumos\n\n2 - Agregar Insumos\n\n3 - Entregar Insumos\n\n4 - Eliminar insumos\n\n");
+            Console.WriteLine("Para salir presiona 'ESC'");
+        }
+
+        static void ListarInsumos()
+        {
+            Console.Clear();
+            Console.WriteLine("Listando Insumos");
+            foreach (Insumos item in InsumosINformaticos)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.ReadKey();
+        }
+        static void AgregarInsumos()
+        {
+            Console.Clear();
+            Console.WriteLine("Agregado Insumos");
+            Console.ReadKey();
+            
+        }
+
+        static void EntregarInsumos()
+        {
+            Console.Clear();
+            Console.WriteLine("Entregado Insumos");
+            Console.ReadKey();
+            
+        }
+
+        static void EliminarInsumos()
+        {
+            Console.Clear();
+            Console.WriteLine("Eliminando Insumos");
+            Console.ReadKey();
         }
     }
 }
